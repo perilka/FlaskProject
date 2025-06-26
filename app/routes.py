8ec0853a4454abbeec83220efe12bbfaab4b97c8
perilka <lisetskaya.evgeniya@yandex.ru>
2025-06-26 22:43:25 +0300

    upd

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+
+@app.route("/hello")
+def hello():
+    return "Hello, world!"
+
+@app.route("/info")
+def info():
+    return "This is an informational page."
+
+# и все оставшиеся маршруты
