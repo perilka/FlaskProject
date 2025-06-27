75f49b87dab5b29eec035d3cae33b816a7304844
perilka <lisetskaya.evgeniya@yandex.ru>
2025-06-27 13:44:58 +0300

    upd

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
+from flask import render_template
 from app import app
 
-@app.route("/hello")
-def hello():
-    return "Hello, world!"
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route("/info")
-def info():
-    return "This is an informational page."
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
